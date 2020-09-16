d75f274dfcc0984fe313eb9512aac4395863d651
Ngo Quang Trung <ngo.quang.trungb@sun-asterisk.com>
2020-09-16 15:15:24 +0700

    bai tap 3

--- lib/Content.dart
+++ lib/Content.dart
@@ -1,11 +1,11 @@
 class Content {
   static const String name = "Mr. Ngo Quang Trung";
-  static const String title = "Developer";
+  static const String title = "Heart Specialist";
   static const String description = "Dr. Stefeni Albert is a cardiologist in Nashville & affiliated with multiple hospitals in the  area.He received his medical degree from Duke University School of Medicine and has been in practice for more than 20 years. ";
 }
 
 class ImageAsset {
-  static const String doctorImg = "assets/doctor_pic2.png";
+  static const String doctor2Img = "assets/doctor_pic2.png";
   static const String emailImg = "assets/email.png";
   static const String callImg = "assets/call.png";
   static const String videoImg = "assets/video_call.png";
@@ -13,4 +13,6 @@ class ImageAsset {
   static const String mapImg = "assets/map.png";
   static const String listImg = "assets/list.png";
   static const String mappinImg = "assets/mappin.png";
+  static const String doctorImg = "assets/doctor.png";
+  static const String heartImg = "assets/heart-specialist.png";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ContactInfo.dart';
+
+import 'HomeScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,11 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      //debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ContactInfo(),
+      home: HomeScreen(),
     );
   }
 }
